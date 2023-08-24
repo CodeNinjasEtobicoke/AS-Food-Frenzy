251b4616ccd1100e7232d6822d1317c40f77c808
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2023-08-24 18:01:54 -0400

    Food Frenzy
    Not done
    
    Sea Farty

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Level level;
+
+    public Text remainingText;
+    public Text remainingSubText;
+    public Text targetText;
+    public Text targetSubText;
+    public Text scoreText;
+
+    public Image[] stars;
+
+    private int starIndex;
+    private bool isGameOver;
+
+    private void Start()
+    {
+        UpdateStars();
+    }
+
+    public void UpdateStars()
+    {
+        for (int i = 0; i < stars.Length; i++)
+        {
+            if (i == starIndex)
+            {
+                stars[i].enabled = true;
+            } else
+            {
+                stars[i].enabled = false;
+            }
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        scoreText.text = score.ToString();
+
+        int visibleStar = 0;
+
+        if (score >= level.score1Star && score < level.score2Star)
+        {
+            visibleStar = 1;
+        } 
+        else if (score >= level.score2Star && score < level.score3Star)
+        {
+            visibleStar = 2;
+        }
+        else if (score >= level.score3Star)
+        {
+            visibleStar = 3;
+        }
+
+        starIndex = visibleStar;
+
+        UpdateStars();
+    }
+
+    public void SetTarget(int target)
+    {
+        targetText.text = target.ToString();
+    }
+
+    public void SetRemaining(int remaining)
+    {
+        remainingText.text = remaining.ToString();
+    }
+
+    public void SetRemaining (string remaining)
+    {
+        remainingText.text = remaining;
+    }
+
+    public void SetLevelType(Level.LevelType type)
+    {
+        switch (type)
+        {
+            case Level.LevelType.MOVES:
+                remainingSubText.text = "Moves Remaining";
+                targetSubText.text = "Target Score";
+                break;
+            case Level.LevelType.OBSTACLE:
+                remainingSubText.text = "Moves Remaining";
+                targetSubText.text = "Dishes Remaining";
+                break;
+            case Level.LevelType.TIMER:
+                remainingSubText.text = "Time Remaining";
+                targetSubText.text = "Target Score";
+                break;
+        }
+    }
+
+    public void OnGameWin(int score)
+    {
+        isGameOver = true;
+    }
+
+    public void OnGameLose()
+    {
+        isGameOver = false;
+    }
+}
